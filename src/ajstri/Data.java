ce22781cdefada407ac39e9f7da694fc2dadb40e
ajstri <ajstrimusicandgaming@gmail.com>
2017-01-27 22:37:02 -0500

    00a12

--- src/ajstri/Data.java
+++ src/ajstri/Data.java
@@ -1,7 +1,7 @@
 package ajstri;
 
 public class Data {
-	public static final String TOKEN = "Hidden for GitHub!";
+	public static final String TOKEN = "Hidden for GitHub...";
 	public static String BOT_ID;
 	public static final String cmdPrefix = "/";
 }
