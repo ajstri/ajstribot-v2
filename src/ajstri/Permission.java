81c4ce5306fa039e470672718111a886e9e0ef18
ajstri <ajstrimusicandgaming@gmail.com>
2017-01-27 21:13:38 -0500

    Build 00a07
    Completed basic Permissions
    Added Command.java Interface to begin implementing commands

--- src/ajstri/Permission.java
+++ src/ajstri/Permission.java
@@ -1,5 +1,19 @@
 package ajstri;
 
-public class Permission {
-	// Code
+public enum Permission {
+	Everyone(new String[]{"e"}),
+	AJ(new String[]{"202422697173581824"}),
+	Admins(new String[]{"188813019017379840", "106591269798608896", 
+						"202422697173581824"}),
+	;
+	
+	private String[] p;
+	
+	private Permission(String[] p) {
+		this.p = p;
+	}
+	
+	public String[] getUsers() {
+		return p;
+	}
 }
---
+++ src/command/Command.java
@@ -0,0 +1,10 @@
+package command;
+
+import ajstri.Permission;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public interface Command {
+	public void execute(MessageReceivedEvent e, String[] args);
+	public Permission getValidExecutors();
+	public String getInfo();
+}
