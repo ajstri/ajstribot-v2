b5d393b7cb4d342368e72d8bf6a302d710ebe734
AJStri <thekidlet320@gmail.com>
2017-02-24 12:20:43 -0500

    00a36
    Added separate try-catch blocks in Main.java

--- src/ajstri/Main.java
+++ src/ajstri/Main.java
@@ -24,12 +24,33 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			api = new JDABuilder(AccountType.BOT).setToken(Data.TOKEN).buildBlocking();
-			api.getPresence().setGame(Game.of("/welcome to begin!"));
-			api.addEventListener(new CommandExecutor());
-			api.setAutoReconnect(true);
-			id = api.getSelfUser().getId();
 		} 
 		catch(Exception e) {
+			System.out.println("JDA Builder threw an exception!");
+		}
+		try {
+			api.getPresence().setGame(Game.of("/welcome to begin!"));
+		}
+		catch(Exception e) {
+			System.out.println("getPresence threw an exception!");
+		}
+		try {
+			api.addEventListener(new CommandExecutor());
+		}
+		catch(Exception e) {
+			System.out.println("addEventListener threw an exception!");
+		}
+		try {
+			api.setAutoReconnect(true);
+		}
+		catch(Exception e) {
+			System.out.println("setAutoReconnect threw an exception!");
+		}
+		try {
+			id = api.getSelfUser().getId();
+		}
+		catch(Exception e) {
+			System.out.println("getSelfUser threw an exception!");
 		}
 		cmds.put("help", new HelpCommand());
 		cmds.put("getid", new GetIDCommand());
